3e95a14f4b1b185e8033827f661455a1aef6d419
bdaralan <bdaralan@gmail.com>
2024-08-31 23:17:52 -0700

    Update documentation

--- Sources/CodableMacros/CodingKeyMacro.swift
+++ Sources/CodableMacros/CodingKeyMacro.swift
@@ -1,2 +1,6 @@
+/// A macro that provide custom key for `CodingKeys` enum.
+///
+/// Use this macro when working with ``CodableMacros/Decodable()``, ``CodableMacros/Encodable()``, or ``CodableMacros/Codable()`` macro to provide custom key.
+///
 @attached(peer)
 public macro CodingKey(_ key: StaticString) = #externalMacro(module: "CodableMacrosMacros", type: "CodingKeyMacro")
--- Sources/CodableMacros/DecodableMacro.swift
+++ Sources/CodableMacros/DecodableMacro.swift
@@ -1,3 +1,21 @@
+/// Defines and implements conformance of the `Decodable` protocol.
+///
+/// This macro adds decodable support to a custom type and conforms the type to the `Decodable` protocol.
+///
+/// ``` swift
+/// @Decodable public struct User {
+///
+///     // behaves the same way as Decodable protocol
+///     let id: String
+///
+///     // provides custom key for CodingKeys enum
+///     @CodingKey("user_tag") let username: String
+///
+///     // provides initial value when value is not presented
+///     var biography: String = ""
+/// }
+/// ```
+///
 @attached(member, names: named(CodingKeys), named(init(from:)))
 @attached(extension, conformances: Decodable, names: named(CodingKeys), named(init(from:)))
 public macro Decodable() = #externalMacro(module: "CodableMacrosMacros", type: "DecodableMacro")
